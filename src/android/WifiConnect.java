60e6c0acf3acc8b0b5798efe85d4261e4df822e0
gorakope <goran.kopevski@polarcape.com>
2014-06-05 15:52:00 +0200

    added correct logic

--- src/android/WifiConnect.java
+++ src/android/WifiConnect.java
@@ -7,21 +7,22 @@ import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
+import org.json.JSONObject;
 
 import android.content.Context;
 import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiManager;
 
-public class WiFiConnect extends CordovaPlugin {
+public class WifiConnect extends CordovaPlugin {
     
-	public WiFiConnect() {
+	public WifiConnect() {
 	}
 	
 	@Override
 	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
 		if (action.equals("connectTo")) {
 			Context context = cordova.getActivity().getApplicationContext();
-			PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, connect("PolarCape",context));
+			PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, connect(args,context));
 			pluginResult.setKeepCallback(true);
 			callbackContext.sendPluginResult(pluginResult);
 			return true;
@@ -29,18 +30,20 @@ public class WiFiConnect extends CordovaPlugin {
 		return false;
 	}
     
-    boolean connect (String ssidName,Context context) {
+    boolean connect (JSONArray args,Context context) throws JSONException {
 	    boolean result = false;
 	    WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
 	    List<WifiConfiguration> arraylist = wifiManager.getConfiguredNetworks();
-	    for (WifiConfiguration wifiConfiguration : arraylist) {
-	        String wifiConfigSSID = wifiConfiguration.SSID.replace("\"", "");
-	        if (wifiConfigSSID.equals(ssidName)) {
-	            result = wifiManager.enableNetwork(wifiConfiguration.networkId, true);
-	            return result;
-	        }
+	    for(int i=0;i<args.length();i++){
+	    	JSONObject json = (JSONObject) args.get(i);
+	    	for (WifiConfiguration wifiConfiguration : arraylist) {
+		        String wifiConfigSSID = wifiConfiguration.SSID.replace("\"", "");
+		        if (wifiConfigSSID.equals(json.get("SSID"))) {
+		            result = wifiManager.enableNetwork(wifiConfiguration.networkId, true);
+		            return result;
+		        }
+		    }
 	    }
 	    return false;
 	}
-	
 }
