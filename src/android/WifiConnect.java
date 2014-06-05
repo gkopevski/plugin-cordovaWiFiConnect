9120cd535172c6ac48066210fb4c9e073d93ecca
gorakope <goran.kopevski@polarcape.com>
2014-06-05 15:26:51 +0200

    changes to class

--- src/android/WifiConnect.java
+++ src/android/WifiConnect.java
@@ -1,29 +1,27 @@
 package org.apache.cordova.wificonnect;
 
-import java.math.BigInteger;
-import java.net.InetAddress;
+import java.util.List;
 
 import org.apache.cordova.CallbackContext;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
 
 import android.content.Context;
-import android.net.wifi.ScanResult;
+import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiManager;
 
-public class WifiConnect extends CordovaPlugin {
+public class WiFiConnect extends CordovaPlugin {
     
-	public WifiConnect() {
+	public WiFiConnect() {
 	}
 	
 	@Override
 	public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
 		if (action.equals("connectTo")) {
 			Context context = cordova.getActivity().getApplicationContext();
-			PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, connect("PolarCape"));
+			PluginResult pluginResult = new PluginResult(PluginResult.Status.OK, connect("PolarCape",context));
 			pluginResult.setKeepCallback(true);
 			callbackContext.sendPluginResult(pluginResult);
 			return true;
@@ -31,9 +29,9 @@ public class WifiConnect extends CordovaPlugin {
 		return false;
 	}
     
-    boolean connect (String ssidName) {
+    boolean connect (String ssidName,Context context) {
 	    boolean result = false;
-	    WifiManager wifiManager = (WifiManager) this.getSystemService(Context.WIFI_SERVICE);
+	    WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
 	    List<WifiConfiguration> arraylist = wifiManager.getConfiguredNetworks();
 	    for (WifiConfiguration wifiConfiguration : arraylist) {
 	        String wifiConfigSSID = wifiConfiguration.SSID.replace("\"", "");
